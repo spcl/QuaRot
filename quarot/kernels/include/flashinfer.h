2b1263bf0d1ec9698fe48db49a569d5ee1697cea
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-04-06 02:07:34 +0900

    Update flashinfer.h
    kv_indicies -> kv_indices

--- quarot/kernels/include/flashinfer.h
+++ quarot/kernels/include/flashinfer.h
@@ -6,14 +6,14 @@
 template <int head_dim>
 void FlashInferBatchDecodeKernel_i4(nv_half* o, nv_half* q, void* kv_data,
                                     nv_half2* kv_param, int32_t* kv_indptr,
-                                    int32_t* kv_indicies,
+                                    int32_t* kv_indices,
                                     int32_t* last_page_offset, int num_layers,
                                     int layer_idx, int num_heads, int page_size,
                                     int batch_size);
 
 template <int head_dim>
 void FlashInferInitKvKernel_i4(void* kv_data, nv_half2* kv_param,
-                               int32_t* kv_indptr, int32_t* kv_indicies,
+                               int32_t* kv_indptr, int32_t* kv_indices,
                                int32_t* last_page_offset, void* key,
                                void* value, nv_half2* key_param,
                                nv_half2* value_param, int32_t* seqlen_indptr,
@@ -22,7 +22,7 @@ void FlashInferInitKvKernel_i4(void* kv_data, nv_half2* kv_param,
 
 template <int head_dim>
 void FlashInferAppendKvKernel_i4(void* kv_data, nv_half2* kv_param,
-                                 int32_t* kv_indptr, int32_t* kv_indicies,
+                                 int32_t* kv_indptr, int32_t* kv_indices,
                                  int32_t* last_page_offset, void* key,
                                  void* value, nv_half2* key_param,
                                  nv_half2* value_param, int num_layers,
@@ -33,14 +33,14 @@ void FlashInferAppendKvKernel_i4(void* kv_data, nv_half2* kv_param,
 template <int head_dim>
 void FlashInferBatchDecodeKernel_f16(nv_half* o, nv_half* q, void* kv_data,
                                     nv_half2* kv_param, int32_t* kv_indptr,
-                                    int32_t* kv_indicies,
+                                    int32_t* kv_indices,
                                     int32_t* last_page_offset, int num_layers,
                                     int layer_idx, int num_heads, int page_size,
                                     int batch_size);
 
 template <int head_dim>
 void FlashInferInitKvKernel_f16(void* kv_data, nv_half2* kv_param,
-                               int32_t* kv_indptr, int32_t* kv_indicies,
+                               int32_t* kv_indptr, int32_t* kv_indices,
                                int32_t* last_page_offset, void* key,
                                void* value, nv_half2* key_param,
                                nv_half2* value_param, int32_t* seqlen_indptr,
@@ -49,7 +49,7 @@ void FlashInferInitKvKernel_f16(void* kv_data, nv_half2* kv_param,
 
 template <int head_dim>
 void FlashInferAppendKvKernel_f16(void* kv_data, nv_half2* kv_param,
-                                 int32_t* kv_indptr, int32_t* kv_indicies,
+                                 int32_t* kv_indptr, int32_t* kv_indices,
                                  int32_t* last_page_offset, void* key,
                                  void* value, nv_half2* key_param,
                                  nv_half2* value_param, int num_layers,
